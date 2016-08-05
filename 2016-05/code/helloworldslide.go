c2b455d930cddfe61dc0c66b0a9b5a9ec1372064
Zachary A. Keener <zkeener@leaf.ag>
2016-08-05 12:23:20 -0500

    updated go talk links

---
+++ 2016-05/code/helloworldslide.go
@@ -0,0 +1,16 @@
+Hello World, the slides
+Hello Basics: How to say World
+26 May 2016
+
+Zachary Keener
+Developer at Agrarian Labs
+@Zak_Keener
+blog.leaf.ag
+
+* My first Section
+
+Hello,
+
+*** My first sub-section
+
+world!
